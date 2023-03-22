59e026f6461b776feda54236d405b0c97962b1b0
Kevin Hernandez <kmhs-18@hotmail.com>
2023-03-22 15:15:19 -0300

    create app whit IA ML.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,12 +5,71 @@
 //  Created by Kevin Hernandez on 21/03/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
+   static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    var recomendedBedtime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            return "Sorry, there was a problem calculating your bedtime"
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("When do you want to make up")) {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section (header: Text("Desired amount of sleep")) {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section (header: Text("Daily coffe intake")) {
+                    Stepper(coffeAmount == 1 ? "1 cup" : "\(coffeAmount) cup", value: $coffeAmount, in: 1...20)
+                }
+                
+                Section(header: Text("Recommended bedtime")) {
+                    Text(recomendedBedtime)
+                        .font(.system(size: 40, weight: .bold))
+                        .foregroundColor(.primary)
+                        .padding()
+                        .frame(maxWidth: .infinity, alignment: .center)
+                }
+            }
+            .navigationTitle("BetterRest")
+        }
     }
 }
 
